b96c8fc447de419ea6329b9fef0b64b5642e5c9d
Polina Moisenia <p.moysenia@gmail.com>
2021-03-01 18:02:29 +0300

    Cors updated

--- src/Startup.cs
+++ src/Startup.cs
@@ -10,7 +10,6 @@ using MoodStateApi.Services;
 namespace MoodStateApi {
     public class Startup {
         private const string CORS_POLICY = "DefaultPolicy";
-        public const string CORS_ANY_ORIGIN_POLICY = "AllowPostAnyOrigin";
 
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
@@ -29,19 +28,13 @@ namespace MoodStateApi {
 
             services.AddSingleton<IService, Service>();
 
-
             services.AddCors(o => o.AddPolicy(CORS_POLICY, b => b.AllowAnyOrigin()));
-            services.AddCors(o => o.AddPolicy(CORS_ANY_ORIGIN_POLICY, b => {
-                b.AllowAnyOrigin()
-                    .WithMethods(HttpMethod.Get.Method, HttpMethod.Post.Method, HttpMethod.Options.Method)
-                    .AllowAnyHeader();
-            }));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseRouting();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
-            app.UseCors(CORS_ANY_ORIGIN_POLICY);
+            app.UseCors(CORS_POLICY);
 
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
